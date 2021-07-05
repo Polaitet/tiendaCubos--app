9194736567eed10d93f980365d5ee1988fa99c4e
Polaitet <quiquesanchollacer@gmail.com>
2021-07-05 16:22:29 +0200

    Add comment to web.php

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// Public
+// Public urls
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-// Private
+// Private urls
