65c190d316a37b695213780691ca054c2ef0705e
Polaitet <quiquesanchollacer@gmail.com>
2021-07-16 16:52:58 +0200

    - Añadir rol a la tabla usuarios - Crear tabla slider

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('role')->default(1); //1 - Cliente // 2 - Administrador
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
