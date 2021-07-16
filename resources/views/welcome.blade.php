dcd00cdd5f1234025fa4207736c3016177d75764
Polaitet <quiquesanchollacer@gmail.com>
2021-07-16 16:14:29 +0200

    Final design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,6 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-
     <div class="container mt-4">
         <div class="justify-content-center" style="display: flex">
             <img style="max-width: 50px;" src="https://i.ibb.co/pz38g7M/Sin-t-tulo-1.png" class="" alt="logo">
@@ -40,10 +39,10 @@
                     <img class="d-block w-100" src="https://www.dalpa.es/wp-content/uploads/2019/10/banner-8-rubiks-aniversario2.jpg" alt="First slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="https://blog.kubekings.com/wp-content/uploads/2017/11/BANNER-2X2-BLOG-KUBEKINGS-1024x276.jpg" alt="Second slide">
+                    <img class="d-block w-100" src="https://i.ibb.co/jHMQZNv/Sin-t-tulo-1.png" alt="Second slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="https://blog.kubekings.com/wp-content/uploads/2017/10/BANNER-3X3-BLOG-KUBEKINGS-1-1024x276.jpg" alt="Third slide">
+                    <img class="d-block w-100" src="https://i.ibb.co/4pf8z1H/Sin-t-tulo-2.png" alt="Third slide">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -54,32 +53,110 @@
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a>
+        <br>
         </div>
-        <div class="card-group">
+        <div class="card-group mb-4">
+            <div class="card">
+                <img class="card-img-top" src="https://cdn.kubekings.com/25772-medium_default/sengso-star-spinner.jpg" alt="Card image cap" alt="Card image cap">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">SengSo Star Spinner</h5>
+                    <p class="card-text">El SengSo Star Spinner es un cubo de lo más sencillo y divertido de resolver, es como un Floppy Megaminx 3x3x1, en esta versión tan solo están permitidos los giros de 180 grados en las caras laterales, no es versión super Floppy, por lo que los giros de 90 grados están prohibidos.</p>
+
+                        <button type="button" class="btn btn-primary mt-auto offset-6">Añadir a la cesta</button>
+
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="https://cdn.kubekings.com/17194-medium_default/x-man-volt-square-1-v2-magnetico.jpg" alt="Card image cap">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">X-Man Volt Square-1 V2 Magnético</h5>
+                    <p class="card-text">Este es un SQ-1 de última generación, tiene un aspecto genial además de un giro increíble, es uno de los pocos Square-1 del mercado que viene con imanes de posicionamiento instalados de fábrica.</p>
+
+                        <button type="button" class="btn btn-primary offset-6 mt-auto">Añadir a la cesta</button>
+
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="https://cdn.kubekings.com/21160-medium_default/meilong-pyraminx-m.jpg" alt="Card image cap">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">MeiLong Pyraminx M</h5>
+                    <p class="card-text">Los imanes le dan una sensación magnética muy buena y aumentan considerablemente el rendimiento de este pyraminx.</p>
+                        <button type="button" class="btn btn-primary offset-6 mt-auto">Añadir a la cesta</button>
+                </div>
+            </div>
+        </div>
+        <div class="card-group mb-4">
             <div class="card">
                 <img class="card-img-top" src="https://kubekings.fr/25714-home_default/x-man-tornado-v2-3x3.jpg" alt="Card image cap" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">xmd tornado v2</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">XMD tornado v2</h5>
+                    <p class="card-text">El X-Man Tornado V2 es el último lanzamiento insignia del Cubo de Rubik 3x3 de QiYi. Es un cubo con un giro muy estable, suave y preciso, además cuenta con un novedoso sistema de ajuste dual y magnético que permite configurar al Tornado V2 M con 125 combinaciones de ajustes totales.</p>
+                        <button type="button" class="btn btn-primary offset-6 mt-auto">Añadir a la cesta</button>
                 </div>
             </div>
             <div class="card">
                 <img class="card-img-top" src="https://cdn.kubekings.com/25670-medium_default/mf8-mater-fto.jpg" alt="Card image cap">
-                <div class="card-body">
+                <div class="card-body d-flex flex-column">
                     <h5 class="card-title">MF8 Mater FTO</h5>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <p class="card-text">El MF8 Mater FTO o octaedro 4x4 es un nuevo cubo de MF8, es un octaedro de 4 capas, el giro es en torno al centro de cada cara por lo que tiene ocho ejes de giro.</p>
+                        <button type="button" class="btn btn-primary  offset-6 mt-auto">Añadir a la cesta</button>
                 </div>
             </div>
             <div class="card">
                 <img class="card-img-top" src="https://cdn.kubekings.com/25686-home_default/lee-mirror-4x4.jpg" alt="Card image cap">
-                <div class="card-body">
+                <div class="card-body d-flex flex-column">
                     <h5 class="card-title">Mirror 4x4</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <p class="card-text">El Lee Mirror 4x4 es una modificación con impresión 3D realizada por Lee, son extensiones impresas que van pegadas al cubo. Su giro es muy bueno y suave.</p>
+                        <button type="button" class="btn btn-primary  offset-6 mt-auto">Añadir a la cesta</button>
                 </div>
             </div>
+        </div>
+        <div class="row">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="fixed-bottom align-self-end" style="margin: 10px">
+                <button type="button" class="btn btn-outline-primary">Subir</button>
+            </div>
+        </div>
+        <div class="mb-4">
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Contacto</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Quienes somos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Metodos de pago</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Ayda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Politica de privacidad</a>
+                </li>
+            </ul>
+        </div>
+
+        <div style="text-align:right">
+            <small>Pagina web registrada con copiright</small>
         </div>
     </div>
 </body>
